d75956e473bdba3e0ea7d818aa16865d6bdea0f2
narenjak <narenjak376@gmail.com>
2022-01-29 20:44:09 +0330

    there is so many bug

--- maz.py
+++ maz.py
@@ -1,7 +1,4 @@
 #make a 2D list for board 
-from importlib.resources import path
-
-
 def initBoard():
     board = [
     [1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1],
@@ -34,15 +31,18 @@ def initBoard():
 #make a structure for each node:
 class node():
     #Constructor
-    def __init__(self,parent = None, pos = None):
-        self.parent = parent #self is a cell :(x,y)
-        self.pos = pos 
+    def __init__(self,parent = None, position = None):
+        self.par = parent 
+        self.pos = position       #cell :(x,y)
         self.g=0
         self.h=0
         self.f=0
     # equalation operator 
     def __eq__(self, operand):
         return self.pos == operand.pos
+
+    def __ne__(self, operand):
+        return self.pos != operand.pos    
 #make dictionery for each value on the board with N S E W value
 #def initMAp():
 #    board_map = {
@@ -50,9 +50,9 @@ class node():
 #    }
 
 #heurestic -> manhatan distance:
-def he(celOne,celTwo):
-    x1,y1=celOne
-    x2,y2=celTwo
+def he(posOne,posTwo):
+    x1,y1=posOne
+    x2,y2=posTwo
     return abs(x1-x2)+abs(y1-y2)
 
 #A* algorithm:    
@@ -64,43 +64,79 @@ def astar (board,start,end):
 
     openls = [st]
     closels = []
-    while len(openls) >0:
+
+    while len(openls) > 0:
         ################find a node:
         #init:
         cur = openls[0]
         cur_index = 0
         #find min:
-        ######################################enumerate?!
-
+        i=0
+        for nd in openls:
+            if nd.f<cur.f:
+                cur = nd
+                cur_index = i
+            i =+ 1    
+        #cur node is min    
         openls.pop(cur)
         closels.append(cur)
         #goal test:
-
+        if cur == end :
+            Rpath_arr = []
+            temp_node = cur
+            while temp_node.par != None :
+                Rpath_arr.append(temp_node.pos)
+                temp_node=temp_node.par
+            path_arr = Rpath_arr[: : -1]
+            return path_arr #goal achived
+        
         #children:
         children_of_cur = []
         for add_node in [(1,0),(0,1),(-1,0),(0,-1)]:
             new_pos = (cur.pos(0)+add_node(0),cur.pos(1)+add_node(1))
             #validation of new pos:
-
-
+            if new_pos[0] > 24 | new_pos[0] < 0 | new_pos[1] > 24 | new_pos[1] < 0 :
+                continue
+            if board(new_pos[0],new_pos[1]) == 0:
+                continue
             #if valid:
             new_node = node(cur,new_pos)
             children_of_cur.append(new_node)
+
         for child_of_cur in children_of_cur:
 
             child_of_cur.h=he(child_of_cur.pos[0],child_of_cur.pos[1])
             child_of_cur.g=cur.g+1
-            child_of_cur.f=child_of_cur.g+child_of_cur.h
+            f=child_of_cur.g+child_of_cur.h
 
+            flag_for_AddToOpn1 = False
+            flag_for_AddToOpn2 = False
+
+            child_existO = node
+            child_existC = node
+            for o in openls:
+                if child_of_cur != o:
+                    flag_for_AddToOpn1 = True
+                    child_existO = child_of_cur
+            for c in closels:
+                if child_of_cur != c:
+                    flag_for_AddToOpn2 = True
+                    child_existC = child_of_cur
+
+            if flag_for_AddToOpn2 and flag_for_AddToOpn1 :
+                openls.append(child_of_cur) 
+                child_of_cur.f = f
+            else:    
+              if flag_for_AddToOpn1 : #child exit on the open list
+                if child_of_cur.f > child_existO.f:
+                    child_of_cur.f=child_existO.f
+
+              if flag_for_AddToOpn2:  #child exit on the close list          
+                if child_of_cur.f > child_existC.f:
+                    child_of_cur.f=child_existC.f
+                    openls.append(child_of_cur)
+                    closels.pop(child_of_cur)
             openls.append(child_of_cur)
-
-
-    #return path
-
-        
-
-        
-
 
 def main():
     brd = initBoard 
