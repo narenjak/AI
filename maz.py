7ba7e4e6ac87feb5995262e1c2e69d7274c75d41
narenjak <narenjak376@gmail.com>
2022-01-30 17:00:02 +0330

    bug fixed and noe its working

--- maz.py
+++ maz.py
@@ -1,6 +1,123 @@
-#make a 2D list for board 
-def initBoard():
-    board = [
+#make a structure for each node:
+class Node():
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position #(x,y)
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other
+
+#heurestic -> manhatan distance:
+def he(posOne,posTwo):
+    x1,y1=posOne
+    x2,y2=posTwo
+    return abs(x1-x2)+abs(y1-y2)
+
+#A* algorithm:    
+def astar (board,start,end):
+    nodes =1 #number of expanded nodes
+    
+    st = Node(None,start) #init start node
+    st.g=0
+    st.h=he(start,end)
+    st.f=st.g+st.h
+    
+    openls = [st]
+    closels = []
+
+    while len(openls) > 0:
+        ################find a node:
+        #init:
+        cur = openls[0]
+        cur_index = 0
+        #find min:
+        i=0
+        for nd in openls:
+            if nd.f<cur.f:
+                cur = nd
+                cur_index = i
+            i =+ 1    
+        #cur node is min    
+        openls.pop(cur_index) 
+        closels.append(cur)
+        #goal test:
+        if cur.position == end :
+            Rpath_arr = []
+            temp_node = cur
+            while temp_node.parent != None :
+                Rpath_arr.append(temp_node.position)
+                temp_node=temp_node.parent
+            Rpath_arr.append(temp_node.position)
+            path_arr = Rpath_arr[: : -1]
+            print ('expanded nodes = '+ str(nodes))
+            return path_arr #goal achived
+        
+        #children:
+        children_of_cur = []
+        for child_node in [(1,0),(0,1),(-1,0),(0,-1)]:
+            child_pos = (cur.position[0]+child_node[0],cur.position[1]+child_node[1])
+            #validation of new pos:
+            if child_pos[0] > 24 or child_pos[0] < 0 or child_pos[1] > 24 or child_pos[1] < 0 :
+                continue
+            if board[child_pos[0]][child_pos[1]] != 1:
+                continue
+            #if valid:
+            new_node = Node(cur,child_pos)
+            children_of_cur.append(new_node)
+        #for child_of_cur in children_of_cur:
+        #    print (child_of_cur.position)     
+        for child_of_cur in children_of_cur:
+
+            child_of_cur.h=he(child_of_cur.position,end)
+            child_of_cur.g=cur.g+1
+            f_child_of_cur=child_of_cur.g+child_of_cur.h
+
+            flag_notexist_childO = True
+            flag_notexist_childC = True
+
+            child_existO_f = Node()
+            child_existC_f = Node()
+
+            for o in openls:
+                if child_of_cur == o:
+                    flag_notexist_childO = False
+                    child_existO_f.f = child_of_cur.f
+
+            counter = 0
+            index_child_of_cur = 0   
+            for c in closels:
+                if child_of_cur == c:
+                    flag_notexist_childC = False
+                    child_existC_f.f = child_of_cur.f
+                    index_child_of_cur = counter
+
+
+            if flag_notexist_childO and flag_notexist_childC :
+                openls.append(child_of_cur) 
+                nodes += 1
+                child_of_cur.f = f_child_of_cur
+
+
+            else:    
+              if flag_notexist_childO : #child exit on the open list
+                if child_of_cur.f > child_existO_f.f:
+                    child_of_cur.f=child_existO_f.f
+
+              if flag_notexist_childC:  #child exit on the close list          
+                if child_of_cur.f > child_existC_f.f:
+                    child_of_cur.f=child_existC_f.f
+                    openls.append(child_of_cur)
+                    nodes += 1
+                    closels.pop(index_child_of_cur)
+
+            #openls.append(child_of_cur)
+    return None
+
+def main():
+    maze = [
     [1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1],
     [0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [0,0,0,0,0,1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1],
@@ -27,125 +144,29 @@ def initBoard():
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ]
-    return board
-#make a structure for each node:
-class node():
-    #Constructor
-    def __init__(self,parent = None, position = None):
-        self.par = parent 
-        self.pos = position       #cell :(x,y)
-        self.g=0
-        self.h=0
-        self.f=0
-    # equalation operator 
-    def __eq__(self, operand):
-        return self.pos == operand.pos
 
-    def __ne__(self, operand):
-        return self.pos != operand.pos    
-#make dictionery for each value on the board with N S E W value
-#def initMAp():
-#    board_map = {
-#        (0,0) : {'N' : 0 ,'S' : 0 ,'E' : 0 ,'W' : 0  },
-#    }
-
-#heurestic -> manhatan distance:
-def he(posOne,posTwo):
-    x1,y1=posOne
-    x2,y2=posTwo
-    return abs(x1-x2)+abs(y1-y2)
-
-#A* algorithm:    
-def astar (board,start,end):
-    st = node(None,start) #init start node
-    st.g=0
-    ed = node (None,end) #init dist node
-    ed.g=ed.h=ed.f=0
-
-    openls = [st]
-    closels = []
-
-    while len(openls) > 0:
-        ################find a node:
-        #init:
-        cur = openls[0]
-        cur_index = 0
-        #find min:
-        i=0
-        for nd in openls:
-            if nd.f<cur.f:
-                cur = nd
-                cur_index = i
-            i =+ 1    
-        #cur node is min    
-        openls.pop(cur)
-        closels.append(cur)
-        #goal test:
-        if cur == end :
-            Rpath_arr = []
-            temp_node = cur
-            while temp_node.par != None :
-                Rpath_arr.append(temp_node.pos)
-                temp_node=temp_node.par
-            path_arr = Rpath_arr[: : -1]
-            return path_arr #goal achived
+    print('A:')
+    start = (13, 2)
+    goal  = (5, 23)
+    path = astar(maze, start, goal)
+    print(path)
+    print('path cost: ' + str(len(path)-1))
+    print('B:')
+    start = (13, 2)
+    goal  = (3,  2)
+    path = astar(maze, start, goal)
+    print(path)
+    print('path cost: ' + str(len(path)-1))
+    print('C:')
+    start = (0 , 0)
+    goal  = (24,24)
+    path = astar(maze, start, goal)
+    try:
+        print(path)
+        print('path cost: ' + str(len(path)-1))
+    except:
+        print('no solution')
         
-        #children:
-        children_of_cur = []
-        for add_node in [(1,0),(0,1),(-1,0),(0,-1)]:
-            new_pos = (cur.pos(0)+add_node(0),cur.pos(1)+add_node(1))
-            #validation of new pos:
-            if new_pos[0] > 24 | new_pos[0] < 0 | new_pos[1] > 24 | new_pos[1] < 0 :
-                continue
-            if board(new_pos[0],new_pos[1]) == 0:
-                continue
-            #if valid:
-            new_node = node(cur,new_pos)
-            children_of_cur.append(new_node)
 
-        for child_of_cur in children_of_cur:
-
-            child_of_cur.h=he(child_of_cur.pos[0],child_of_cur.pos[1])
-            child_of_cur.g=cur.g+1
-            f=child_of_cur.g+child_of_cur.h
-
-            flag_for_AddToOpn1 = False
-            flag_for_AddToOpn2 = False
-
-            child_existO = node
-            child_existC = node
-            for o in openls:
-                if child_of_cur != o:
-                    flag_for_AddToOpn1 = True
-                    child_existO = child_of_cur
-            for c in closels:
-                if child_of_cur != c:
-                    flag_for_AddToOpn2 = True
-                    child_existC = child_of_cur
-
-            if flag_for_AddToOpn2 and flag_for_AddToOpn1 :
-                openls.append(child_of_cur) 
-                child_of_cur.f = f
-            else:    
-              if flag_for_AddToOpn1 : #child exit on the open list
-                if child_of_cur.f > child_existO.f:
-                    child_of_cur.f=child_existO.f
-
-              if flag_for_AddToOpn2:  #child exit on the close list          
-                if child_of_cur.f > child_existC.f:
-                    child_of_cur.f=child_existC.f
-                    openls.append(child_of_cur)
-                    closels.pop(child_of_cur)
-            openls.append(child_of_cur)
-
-def main():
-    brd = initBoard 
-    print(brd)
-    sart_node=(2,13)
-    goal_node=(2,3)
-    #path = astar(brd,sart_node,goal_node)
-    #a shape with 0 1 s(start) e1(goal1) e2(goal2) and *(path)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
